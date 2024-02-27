c40a4ca68da358119e5180a9739eff1230f72d6a
miratwebpr <webmiratfl@gmail.com>
2024-02-27 16:12:25 -0600

    vpc test git add .git add .git add .git add .git add .git add .git add .

--- eks.tf
+++ eks.tf
@@ -1,206 +1,208 @@
 
-# Role for cluster
-resource "aws_iam_role" "eks" {
-    name = "eks-cluster"
-    assume_role_policy = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "eks.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
-        }
-    ]
-}
-POLICY
-}
+# # Role for cluster
+# resource "aws_iam_role" "eks" {
+#     name = "eks-cluster"
+#     assume_role_policy = <<POLICY
+# {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Effect": "Allow",
+#             "Principal": {
+#                 "Service": "eks.amazonaws.com"
+#             },
+#             "Action": "sts:AssumeRole"
+#         }
+#     ]
+# }
+# POLICY
+# }
 
-resource "aws_iam_role_policy_attachment" "eks" {
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-    role       = aws_iam_role.eks.name
-}
+# resource "aws_iam_role_policy_attachment" "eks" {
+#     policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+#     role       = aws_iam_role.eks.name
+# }
 
-resource "aws_eks_cluster" "main" {
-    name = "main"
-    role_arn = aws_iam_role.eks.arn
+# resource "aws_eks_cluster" "main" {
+#     name = "main"
+#     role_arn = aws_iam_role.eks.arn
 
-    vpc_config {
-      subnet_ids = concat(aws_subnet.public.*.id, aws_subnet.private.*.id)
-    }
+#     version = "1.27"
 
-    depends_on = [aws_iam_role_policy_attachment.eks]
-}
+#     vpc_config {
+#       subnet_ids = concat(aws_subnet.public.*.id, aws_subnet.private.*.id)
+#     }
 
-
-# Roles for nodes
-
-
-resource "aws_iam_role" "nodes" {
-  name = "eks-node-group-nodes"
-
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "nodes-AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.nodes.name
-}
-
-resource "aws_iam_role_policy_attachment" "nodes-AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.nodes.name
-}
-
-resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.nodes.name
-}
-
-resource "aws_eks_node_group" "private-nodes" {
-  cluster_name    = aws_eks_cluster.main.name
-  node_group_name = "private-nodes"
-  node_role_arn   = aws_iam_role.nodes.arn
-
-  subnet_ids = aws_subnet.private.*.id
-
-  scaling_config {
-    desired_size = 1
-    max_size     = 5
-    min_size     = 0
-  }
-
-  labels = {
-    role = "general"
-  }
+#     depends_on = [aws_iam_role_policy_attachment.eks]
+# }
 
 
-  launch_template {
-    name    = aws_launch_template.eks-with-disks.name
-    version = aws_launch_template.eks-with-disks.latest_version
-  }
+# # Roles for nodes
 
-  depends_on = [
-    aws_iam_role_policy_attachment.nodes-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.nodes-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
-  ]
-}
 
-resource "aws_launch_template" "eks-with-disks" {
-  name = "eks-with-disks"
+# resource "aws_iam_role" "nodes" {
+#   name = "eks-node-group-nodes"
 
-  instance_type = var.instance
+#   assume_role_policy = jsonencode({
+#     Statement = [{
+#       Action = "sts:AssumeRole"
+#       Effect = "Allow"
+#       Principal = {
+#         Service = "ec2.amazonaws.com"
+#       }
+#     }]
+#     Version = "2012-10-17"
+#   })
+# }
 
-  image_id = var.ami_id
+# resource "aws_iam_role_policy_attachment" "nodes-AmazonEKSWorkerNodePolicy" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+#   role       = aws_iam_role.nodes.name
+# }
 
-  block_device_mappings {
-    device_name = "/dev/xvdb"
+# resource "aws_iam_role_policy_attachment" "nodes-AmazonEKS_CNI_Policy" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+#   role       = aws_iam_role.nodes.name
+# }
 
-    ebs {
-      volume_size = 20
-      volume_type = "gp3"
-    }
-  }
-}
+# resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadOnly" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+#   role       = aws_iam_role.nodes.name
+# }
 
-data "tls_certificate" "eks" {
-  url = aws_eks_cluster.main.identity[0].oidc[0].issuer
-}
+# resource "aws_eks_node_group" "private-nodes" {
+#   cluster_name    = aws_eks_cluster.main.name
+#   node_group_name = "private-nodes"
+#   node_role_arn   = aws_iam_role.nodes.arn
 
-resource "aws_iam_openid_connect_provider" "eks" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
-  url             = aws_eks_cluster.main.identity[0].oidc[0].issuer
-}
+#   subnet_ids = aws_subnet.private.*.id
 
-data "aws_iam_policy_document" "test_oidc_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-    effect  = "Allow"
+#   scaling_config {
+#     desired_size = 1
+#     max_size     = 5
+#     min_size     = 0
+#   }
 
-    condition {
-      test     = "StringEquals"
-      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
-      values   = ["system:serviceaccount:default:aws-test"]
-    }
+#   labels = {
+#     role = "general"
+#   }
 
-    principals {
-      identifiers = [aws_iam_openid_connect_provider.eks.arn]
-      type        = "Federated"
-    }
-  }
-}
 
-resource "aws_iam_role" "test_oidc" {
-  assume_role_policy = data.aws_iam_policy_document.test_oidc_assume_role_policy.json
-  name               = "test-oidc"
-}
+#   launch_template {
+#     name    = aws_launch_template.eks-with-disks.name
+#     version = aws_launch_template.eks-with-disks.latest_version
+#   }
 
-resource "aws_iam_policy" "test-policy" {
-  name = "test-policy"
+#   depends_on = [
+#     aws_iam_role_policy_attachment.nodes-AmazonEKSWorkerNodePolicy,
+#     aws_iam_role_policy_attachment.nodes-AmazonEKS_CNI_Policy,
+#     aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
+#   ]
+# }
 
-  policy = jsonencode({
-    Statement = [{
-      Action = [
-        "s3:ListAllMyBuckets",
-        "s3:GetBucketLocation"
-      ]
-      Effect   = "Allow"
-      Resource = "arn:aws:s3:::*"
-    }]
-    Version = "2012-10-17"
-  })
-}
+# resource "aws_launch_template" "eks-with-disks" {
+#   name = "eks-with-disks"
 
-resource "aws_iam_role_policy_attachment" "test_attach" {
-  role       = aws_iam_role.test_oidc.name
-  policy_arn = aws_iam_policy.test-policy.arn
-}
+#   instance_type = var.instance
 
-output "test_policy_arn" {
-  value = aws_iam_role.test_oidc.arn
-}
+#   image_id = var.ami_id
 
-# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
-data "aws_iam_policy" "ebs_csi_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-}
+#   block_device_mappings {
+#     device_name = "/dev/xvdb"
 
-module "irsa-ebs-csi" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "4.7.0"
+#     ebs {
+#       volume_size = 20
+#       volume_type = "gp3"
+#     }
+#   }
+# }
 
-  create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-${aws_eks_cluster.main.name}"
-  provider_url                  = aws_eks_cluster.main.identity[0].oidc[0].issuer
-  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-  depends_on = [aws_eks_node_group.private-nodes]
-}
+# data "tls_certificate" "eks" {
+#   url = aws_eks_cluster.main.identity[0].oidc[0].issuer
+# }
 
-resource "aws_eks_addon" "ebs-csi" {
-  cluster_name             = aws_eks_cluster.main.name
-  addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.20.0-eksbuild.1"
-  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-  tags = {
-    "eks_addon" = "ebs-csi"
-    "terraform" = "true"
-  }
-  depends_on = [aws_eks_node_group.private-nodes]
-}
+# resource "aws_iam_openid_connect_provider" "eks" {
+#   client_id_list  = ["sts.amazonaws.com"]
+#   thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+#   url             = aws_eks_cluster.main.identity[0].oidc[0].issuer
+# }
+
+# data "aws_iam_policy_document" "test_oidc_assume_role_policy" {
+#   statement {
+#     actions = ["sts:AssumeRoleWithWebIdentity"]
+#     effect  = "Allow"
+
+#     condition {
+#       test     = "StringEquals"
+#       variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+#       values   = ["system:serviceaccount:default:aws-test"]
+#     }
+
+#     principals {
+#       identifiers = [aws_iam_openid_connect_provider.eks.arn]
+#       type        = "Federated"
+#     }
+#   }
+# }
+
+# resource "aws_iam_role" "test_oidc" {
+#   assume_role_policy = data.aws_iam_policy_document.test_oidc_assume_role_policy.json
+#   name               = "test-oidc"
+# }
+
+# resource "aws_iam_policy" "test-policy" {
+#   name = "test-policy"
+
+#   policy = jsonencode({
+#     Statement = [{
+#       Action = [
+#         "s3:ListAllMyBuckets",
+#         "s3:GetBucketLocation"
+#       ]
+#       Effect   = "Allow"
+#       Resource = "arn:aws:s3:::*"
+#     }]
+#     Version = "2012-10-17"
+#   })
+# }
+
+# resource "aws_iam_role_policy_attachment" "test_attach" {
+#   role       = aws_iam_role.test_oidc.name
+#   policy_arn = aws_iam_policy.test-policy.arn
+# }
+
+# output "test_policy_arn" {
+#   value = aws_iam_role.test_oidc.arn
+# }
+
+# # https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
+# data "aws_iam_policy" "ebs_csi_policy" {
+#   arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+# }
+
+# module "irsa-ebs-csi" {
+#   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+#   version = "4.7.0"
+
+#   create_role                   = true
+#   role_name                     = "AmazonEKSTFEBSCSIRole-${aws_eks_cluster.main.name}"
+#   provider_url                  = aws_eks_cluster.main.identity[0].oidc[0].issuer
+#   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+#   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+#   depends_on = [aws_eks_node_group.private-nodes]
+# }
+
+# resource "aws_eks_addon" "ebs-csi" {
+#   cluster_name             = aws_eks_cluster.main.name
+#   addon_name               = "aws-ebs-csi-driver"
+#   addon_version            = "v1.20.0-eksbuild.1"
+#   service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+#   tags = {
+#     "eks_addon" = "ebs-csi"
+#     "terraform" = "true"
+#   }
+#   depends_on = [aws_eks_node_group.private-nodes]
+# }
 
 
 
