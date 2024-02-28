858e08906c0dca9aee8fcdc8657455938dbf4ef0
miratwebpr <webmiratfl@gmail.com>
2024-02-27 19:37:20 -0600

    cluster name fixeD

--- eks.tf
+++ eks.tf
@@ -44,7 +44,7 @@ module "irsa-ebs-csi" {
 }
 
 resource "aws_eks_addon" "ebs-csi" {
-  cluster_name             = "module.eks.${local.cluster_name}"
+  cluster_name             = local.cluster_name
   addon_name               = "aws-ebs-csi-driver"
   addon_version            = "v1.20.0-eksbuild.1"
   service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
